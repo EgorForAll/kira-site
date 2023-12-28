ae126bcdbfbffffca76e99a7450910d10f35813c
EgorForAll <eia.web.ss@gmail.com>
2023-12-28 10:48:57 +0300

    favicon added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{url('resources/assets/images/favicon/Favicon.png')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{url('/storage/images/favicon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{url('/storage/images/favicon/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{url('/storage/images/favicon/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{url('/storage/images/favicon/site.webmanifest')}}">
+    <link rel="mask-icon" href="{{url('/storage/images/favicon/safari-pinned-tab.svg')}}" color="#5bbad5">
+    <link rel="shortcut icon" href="{{url('/storage/images/favicon/favicon.ico')}}">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="{{url('/storage/images/favicon/browserconfig.xml')}}">
+    <meta name="theme-color" content="#ffffff">
     <link href="https://cdn.jsdelivr.net/gh/yesiamrocks/cssanimation.io@1.0.3/cssanimation.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <title>Блог Киры Ивановой</title>
-    @vite('resources/js/app.js')
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body>
     <div id="app">
